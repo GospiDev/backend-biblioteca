b38c80aba128737c7685710cb1281c49532b4a46
Javier Ignacio Riquelme Torres <gospinforma@gmail.com>
2025-10-30 12:06:18 -0300

    fix: arreglos menores

--- routes/libro.routes.js
+++ routes/libro.routes.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// Importamos el controlador para asociar las rutas con su lógica
 const libroController = require('../controllers/libro.controller');
 
-// GET /api/libros -> Obtener todos los libros
 router.get('/', libroController.getLibros);
 
-// POST /api/libros -> Crear un nuevo libro
 router.post('/', libroController.createLibro);
 
-// PUT /api/libros/:id -> Actualizar un libro por su ID
 router.put('/:id', libroController.updateLibro);
 
-// DELETE /api/libros/:id -> Eliminar un libro por su ID
 router.delete('/:id', libroController.deleteLibro);
 
 module.exports = router;
